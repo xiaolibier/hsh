435084ef3ca788130659967630ab9415d3529336
zhangmingyue <dengxihai1234@126.com>
2018-07-02 16:07:38 +0800

    update commons

--- public/libs/base.js
+++ public/libs/base.js
@@ -20,7 +20,8 @@
 	var phoneNumber = "";
 
 	//请求服务地址
-	var serverUrl = "http://www.mijson.com/api/";//
+	//var serverUrl = "http://www.mijson.com/api/";//
+	var serverUrl = "http://localhost/api/";//
 
 	
 	
